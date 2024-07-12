652485f4d2eedea303b8b4a060c444bf11c2c7ee
ahmadou5 <ahmadlasauwal@gmail.com>
2024-07-12 01:34:25 +0100

    1

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+
+export const Loading = () => {
+    return(
+    <div className="inset-0 fixed bg-black bg-opacity-100 w-[100%] z-[99999999] min-h-screen h-auto backdrop-blur-sm flex ">
+        <div className="w-[100%] h-[auto] flex items-center justify-center">
+           <div className="w-auto h-auto mt-4">
+            <img src='./assets/show.png' className="w-[200px] h-[200px]" />
+           </div>
+        </div>
+    </div>
+    )
+}
