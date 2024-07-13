38ea3b2535f4f2f51c6d88bac70c4d2ac9d5f2d0
ahmadou5 <ahmadlasauwal@gmail.com>
2024-07-13 18:42:27 +0100

    day3

--- src/hooks/useGetBalance.js
+++ src/hooks/useGetBalance.js
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+import { clusterApiUrl, Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 //import { ethers,formatUnits,parseUnits, formatEther } from "ethers";
 //import { Supabase } from "@/Utils/supabasedb";
 import { GlobalContext } from "@/context/AppContext";
@@ -20,12 +20,11 @@ export const useGetUserBalance = () => {
     user,
   } = GlobalContext();
   
-  const [trx, setTrx] = useState(true);
-  const id = user?.initDataUnsafe?.user?.id
+  
   useEffect(() => {
     
     const getUserSolBalance = async () => {
-          const connection = new Connection(providerURL)
+          const connection = new Connection(clusterApiUrl('mainnet-beta'),'confirmed')
           console.log('5555',connection)
           const balance = await connection.getBalance(new PublicKey(userAddress))
           console.log(balance, providerURL, "1 non  blnc");
