fff0bf6d461dc2b16072367fecc7ed125053dea3
ahmadou5 <ahmadlasauwal@gmail.com>
2024-07-14 04:55:40 +0100

    day3-2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
-import { Inter, Medula_One } from "next/font/google";
+import { Inter} from "next/font/google";
 import "./globals.css";
 import { MiniContextProvider } from "@/context/AppContext";
 import Script from "next/script";
-const inter = Medula_One({ subsets: ["latin"], weight: '400' });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Script src="https://terminal.jup.ag/main-v2.js"></Script>
+      
       <Script  src="https://telegram.org/js/telegram-web-app.js"></Script>
       <MiniContextProvider>
         <body className={inter.className}>{children}</body>
