9307b26d49c2bb63cdbb29611c4b9fe84697563a
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-13 10:56:47 +0100

    dday22

--- src/app/api/action/route.js
+++ src/app/api/action/route.js
@@ -1,4 +1,5 @@
-
+import { ACTIONS_CORS_HEADERS } from "@solana/actions"
+import { clusterApiUrl, Connection, PublicKey, SystemProgram, Transaction } from "@solana/web3.js"
 export const GET = async(request) => {
   const response = {
     icon: 'https://earn-fuse.vercel.app/assets/show.png',
@@ -9,17 +10,26 @@ export const GET = async(request) => {
       message:"This is not yet implemented"
     }
   }
-  return Response.json(response)
+  return Response.json(response,{headers:ACTIONS_CORS_HEADERS})
 }
 
 export const POST = async(request) => {
     const ReqBody = await request.json()
     const userPKey = ReqBody.account;
-
+    const connection = new Connection(clusterApiUrl('mainnet-beta'))
+    const tx = new Transaction()
+    tx.feePayer = new PublicKey(userPKey)
+    tx.recentBlockhash = (await connection.getLatestBlockhash({commitment: 'finalized'})).blockhash
+    const serialTX = tx.serialize({requireAllSignatures:false, verifySignatures:false}).toString('base64')
     console.log(userPKey,'account ne')
     const response = {
-      transaction: '',
-      message: `Hello ${userPKey}`
+      transaction: serialTX,
+      message: `Hello from ${userPKey}`
     }
-    return Response.json(response)
+    
+    return Response.json(response,{headers:ACTIONS_CORS_HEADERS})
+}
+
+export const OPTIONS = async(request) => {
+  return new Response(null,{headers:ACTIONS_CORS_HEADERS})
 }
